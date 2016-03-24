84412ebb5f4b18e1a66bcd544377fed4cc169b75
Mazurkevich350501 <kiramzit13@gmail.com>
2016-03-24 18:27:41 +0200

    add

---
+++ EventCloud.Web/App/Main/views/creatives/createDialog.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    angular.module('app').controller('app.views.creatives.createDialog', [
+        'abp.services.app.creative', '$modalInstance',
+        function (creativService, $modalInstance) {
+            var vm = this;
+
+            vm.creativ = {
+                Title: 'popopo',
+                //description: '',
+                date: moment().add('day', 1).format('YYYY-MM-DD'),
+                maxRegistrationCount: 0
+            }
+
+            vm.save = function() {
+                creativService
+                    .Create(vm.creativ)
+                    .success(function () {
+                        abp.notify.success("Successfully saved.");
+                        $modalInstance.close();
+                    });
+            };
+
+            vm.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        }
+    ]);
+})();
