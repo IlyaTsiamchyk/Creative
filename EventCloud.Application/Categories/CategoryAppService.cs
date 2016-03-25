d0b4f6d3d687166990989b5c2affdb28c2b72201
ilia.co@mail.ru <ilia.co@mail.ru>
2016-03-25 12:24:42 +0300

    AppServices add

---
+++ EventCloud.Application/Categories/CategoryAppService.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Abp.UI;
+using EventCloud.Core.Entities;
+using System.Threading.Tasks;
+
+namespace EventCloud.Categories
+{
+    public class CategoryAppService : ICategoryAppService
+    {
+        private readonly IRepository<Category, int> _categoryRepository;
+
+        public CategoryAppService(
+            IRepository<Category, int> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Category> Details(int id)
+        {
+            var category = await _categoryRepository.FirstOrDefaultAsync(id);
+
+            if (category == null)
+            {
+                throw new UserFriendlyException("Could not found the category.");
+            }
+
+            return category;
+        }
+    }
+}
---
+++ EventCloud.Application/Categories/Dtos/CategoryListDto.cs
@@ -0,0 +1,12 @@
+﻿using EventCloud.Creatives;
+using System.Collections.Generic;
+
+namespace EventCloud.Categories
+{
+    public class CategoryListDto
+    {
+        public virtual string Name { get; set; }
+        public virtual IList<Creative> Creatives { get; set; }
+        public virtual string Url { get; set; }
+    }
+}
