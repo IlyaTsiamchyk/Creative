372cf843ceb32fb414c24fc59fe973b9f582eccd
Mazurkevich350501 <kiramzit13@gmail.com>
2016-04-01 19:10:13 +0200

    save chapters

--- EventCloud.Web/App/Main/views/editor/index.js
+++ EventCloud.Web/App/Main/views/editor/index.js
@@ -4,7 +4,7 @@
         '$scope', '$modal', '$stateParams', 'abp.services.app.creative', 'abp.services.app.session',
         function ($scope, $modal, $stateParams, creativesService, sessionService) {
             var vm = this;
-
+            vm.creative;
             creativesService.details($stateParams.id).success(function (result) {
                 var creative = jQuery.parseJSON(result);
                 console.log(creative);
@@ -19,9 +19,8 @@
                             })
                         }
                         else {
-                            throw {
-                                message: "No eccess!"
-                            }
+                            abp.message.error("No eccess!", 'saasdasdasd');
+
                         }
                     });
                 }
@@ -38,29 +37,36 @@
             };
 
             vm.saveCreative = function () {
-                if (vm.creative.Title === '')
-                    throw {
-                        message: "Empty creative title"
-                    }
-                vm.creative.Chapters.forEach(function (chapter, i, chapters) {
-                    chapter.Number = i;
-                    if (chapter.Name === '') throw {
-                        message: "Empty chapter name"
-                    }
-                });
-                
+                try {
+                    if (vm.creative.Title === '')
+                        throw {
+                            message: "Empty creative title"
+                        }
+                    vm.creative.Chapters.forEach(function (chapter, i, chapters) {
+                        chapter.Number = i;
+                        if (chapter.Name === '')
+                            throw {
+                                message: "Empty chapter name"
+                            }
+                        creativesService.edit(vm.creative);
+                        console.log(vm.creative);
+                    });
+                } catch (exp) {
+                    abp.message.error("", "Empty chapter name");
+                }
             };
 
             vm.removeCreative = function () {
-                
+
             }
 
             vm.newChapter = function () {
                 vm.creative.Chapters.push({
                     Name: "New Chapter",
+                    CreativeId: vm.creative.Id,
                     Content: ''
                 });
-                setDrag();
+                //setDrag();
             }
 
             vm.removeChapter = function () {
@@ -76,63 +82,6 @@
 
 
             //////////////////////////////////////////////////////////
-            function handleDragStart(e) {
-                console.log("1");
-                this.style.opacity = '0.4';  // this / e.target is the source node.
-            }
-
-            function handleDragOver(e) {
-                if (e.preventDefault) {
-                    e.preventDefault(); // Necessary. Allows us to drop.
-                }
-
-                e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
-                return false;
-            }
-
-            function handleDragEnter(e) {
-                // this / e.target is the current hover target.
-                console.log("3");
-                this.classList.add('over');
-            }
-
-            function handleDragLeave(e) {
-                console.log("4");
-                this.classList.remove('over');  // this / e.target is previous target element.
-            }
-
-            function handleDrop(e) {
-                // this / e.target is current target element.
-
-                if (e.stopPropagation) {
-                    e.stopPropagation(); // stops the browser from redirecting.
-                }
-
-                // See the section on the DataTransfer object.
-
-                return false;
-            }
-
-            function handleDragEnd(e) {
-                // this/e.target is the source node.
-
-                [].forEach.call(cols, function (col) {
-                    col.classList.remove('over');
-                });
-            }
-
-            function setDrag() {
-                var cols = document.querySelectorAll('#chapters .dropdown');
-                [].forEach.call(cols, function (col) {
-                    col.addEventListener('dragstart', handleDragStart, false);
-                    col.addEventListener('dragenter', handleDragEnter, false)
-                    col.addEventListener('dragover', handleDragOver, false);
-                    col.addEventListener('dragleave', handleDragLeave, false);
-                    col.addEventListener('drop', handleDrop, false);
-                    col.addEventListener('dragend', handleDragEnd, false);
-                });
-            }
         }
     ]);
 })();
