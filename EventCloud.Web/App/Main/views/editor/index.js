f035c98e9bf93ca4e0e84852385e1f58b64eacd2
Mazurkevich350501 <kiramzit13@gmail.com>
2016-04-01 17:52:35 +0200

    Add startRating

--- EventCloud.Web/App/Main/views/editor/index.js
+++ EventCloud.Web/App/Main/views/editor/index.js
@@ -1,62 +1,137 @@
 ﻿(function () {
     var controllerId = 'app.views.editor.indexx';
     angular.module('app').controller(controllerId, [
-        '$scope', '$modal', 'abp.services.app.creative', 'abp.services.app.session',
-        function ($scope, $modal, creativesService, sessionService) {
+        '$scope', '$modal', '$stateParams', 'abp.services.app.creative', 'abp.services.app.session',
+        function ($scope, $modal, $stateParams, creativesService, sessionService) {
             var vm = this;
-            vm.globalId = 0;
-            vm.chaptersList = [
-                {
-                    Name: "firts chap",
-                    Content: 'asakjhdshdskjadsfkjdsfjdsfjads',
-                    Number: 1,
-                    Id: 1
-                },
-                {
-                    Name: "sec chap",
-                    Content: 'asdasddfsczxczxcjads',
-                    Number: 3,
-                    Id: 2
-                },
-                {
-                    Name: "thre chap",
-                    Content: 'asadzxczxfjdsfjads',
-                    Number: 5,
-                    Id: 3
-                },
-                {
-                    Name: "fouth chap",
-                    Content: 'asakzxcaddqwadasdasdfkjdsfjdsfjads',
-                    Number: 6,
-                    Id: 4
+
+            creativesService.details($stateParams.id).success(function (result) {
+                var creative = jQuery.parseJSON(result);
+                console.log(creative);
+                if (creative !== null) {
+                    sessionService.getCurrentLoginInformations().success(function (result) {
+                        var sessionInformation = result;
+                        console.log(sessionInformation);
+                        if (sessionInformation.user.id === creative.UserId) {
+                            vm.creative = creative;
+                            vm.creative.Chapters.sort(function (a, b) {
+                                return a.number - b.number;
+                            })
+                        }
+                        else {
+                            throw {
+                                message: "No eccess!"
+                            }
+                        }
+                    });
                 }
-            ]
-            console.log(vm.chaptersList);
-            vm.editorContent = 'qwertyuiop';
-            vm.creativeTitle = 'Kokoko';
-            vm.getChapterContent = function (Id) {
-                Id--;
-                vm.chaptersList[vm.globalId].Content = vm.editorContent;
-                vm.editorContent = vm.chaptersList[Id].Content;
-                vm.globalId = Id;
+                else
+                    throw {
+                        message: "Not found!"
+                    }
+            });
+
+            vm.globalIndex = 0;
+
+            vm.getChapterContent = function (Index) {
+                vm.globalIndex = Index;
             };
+
             vm.saveCreative = function () {
+                if (vm.creative.Title === '')
+                    throw {
+                        message: "Empty creative title"
+                    }
+                vm.creative.Chapters.forEach(function (chapter, i, chapters) {
+                    chapter.Number = i;
+                    if (chapter.Name === '') throw {
+                        message: "Empty chapter name"
+                    }
+                });
                 
             };
-            
+
             vm.removeCreative = function () {
                 
             }
+
             vm.newChapter = function () {
-                vm.chaptersList.push({
+                vm.creative.Chapters.push({
                     Name: "New Chapter",
-                    Content: '',
-                    Number: vm.chaptersList.length + 1,
-                    Id: vm.chaptersList.length + 1
+                    Content: ''
+                });
+                setDrag();
+            }
+
+            vm.removeChapter = function () {
+                var index = 0;
+                if (vm.creative.Chapters !== null) {
+                    while (vm.creative.Chapters[index].Id === vm.globalIndex) {
+                        index++;
+                    }
+                    vm.creative.Chapters.splice(index, 1);
+                }
+            }
+
+
+
+            //////////////////////////////////////////////////////////
+            function handleDragStart(e) {
+                console.log("1");
+                this.style.opacity = '0.4';  // this / e.target is the source node.
+            }
+
+            function handleDragOver(e) {
+                if (e.preventDefault) {
+                    e.preventDefault(); // Necessary. Allows us to drop.
+                }
+
+                e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+                return false;
+            }
+
+            function handleDragEnter(e) {
+                // this / e.target is the current hover target.
+                console.log("3");
+                this.classList.add('over');
+            }
+
+            function handleDragLeave(e) {
+                console.log("4");
+                this.classList.remove('over');  // this / e.target is previous target element.
+            }
+
+            function handleDrop(e) {
+                // this / e.target is current target element.
+
+                if (e.stopPropagation) {
+                    e.stopPropagation(); // stops the browser from redirecting.
+                }
+
+                // See the section on the DataTransfer object.
+
+                return false;
+            }
+
+            function handleDragEnd(e) {
+                // this/e.target is the source node.
+
+                [].forEach.call(cols, function (col) {
+                    col.classList.remove('over');
                 });
             }
-            vm.removeChapter = function () {
-                
+
+            function setDrag() {
+                var cols = document.querySelectorAll('#chapters .dropdown');
+                [].forEach.call(cols, function (col) {
+                    col.addEventListener('dragstart', handleDragStart, false);
+                    col.addEventListener('dragenter', handleDragEnter, false)
+                    col.addEventListener('dragover', handleDragOver, false);
+                    col.addEventListener('dragleave', handleDragLeave, false);
+                    col.addEventListener('drop', handleDrop, false);
+                    col.addEventListener('dragend', handleDragEnd, false);
+                });
             }
         }
     ]);
