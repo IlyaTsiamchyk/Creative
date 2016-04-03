a1f099b09494778fd3ce0bc3eaba29e1e879dfd7
itsiamchyk <itsiamchyk@omertex.com>
2016-04-03 18:58:32 +0300

    Add rate add

--- EventCloud.EntityFramework/EntityFramework/Repositories/CreativeRepository.cs
+++ EventCloud.EntityFramework/EntityFramework/Repositories/CreativeRepository.cs
@@ -9,6 +9,7 @@ using EventCloud.Core.Entities;
 using System.Data.Entity;
 using Abp.EntityFramework;
 using EventCloud.Creatives;
+using System.Data.Entity.Core;
 
 namespace EventCloud.EntityFramework.Repositories
 {
@@ -29,21 +30,23 @@ namespace EventCloud.EntityFramework.Repositories
         {
             Rate r = Context.Rates.Where(rt => rt.UserBy == rate.UserBy).FirstOrDefault<Rate>();
 
-            if (r == null)
-            {
-                Context.Entry(rate).State = System.Data.Entity.EntityState.Added;
-                Context.SaveChanges();
-            }
+            if (r != null) throw new ArgumentException("You are already send your rating.");
+
+            Context.Entry(rate).State = System.Data.Entity.EntityState.Added;
+            Context.SaveChanges();
         }
 
         public void UpdateChapter(Chapter chapter)
         {
             if (chapter.Id != 0)
-                Context.Entry(chapter).State = System.Data.Entity.EntityState.Modified;
+                Context.Entry<Chapter>(chapter).State = System.Data.Entity.EntityState.Modified;
             else
-                Context.Entry(chapter).State = System.Data.Entity.EntityState.Added;
+            {
+                //Context.Entry<Chapter>(chapter).State = System.Data.Entity.EntityState.Added;
+                Context.Chapters.Add(chapter);
+            }
 
-            Context.SaveChanges();            
+            Context.SaveChanges();      
         }
     }
 }
